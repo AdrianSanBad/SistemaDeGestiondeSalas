c9b1f8a1fc97a617b149d4275e5622460e9818cb
adriansb <adriansb2702@gmail.com>
2025-04-29 12:11:25 -0600

    eliminacion ahora con solo cambio de estado

--- EliminarSala.py
+++ EliminarSala.py
@@ -1,28 +1,24 @@
 # import the sql library to connect to the database
 import mysql.connector
+
 # delete the sala with the id passed as parameter  checking if the sala is available (disponible==True)
-def eliminarSala(id,disponible):
-    #if the sala is available (disponible==True) then delete the sala from the database
-    if disponible is True:
-        try:
-            # connect to the database
-            connection = mysql.connector.connect(host='localhost',
-                                                 database='sala', 
-                                                 user='root',
-                                                 password='1234pwc')
-            cursor = connection.cursor()
-            # delete the sala with the id passed as parameter
-            sql_delete_query = """DELETE FROM sala WHERE id = %s"""
-            cursor.execute(sql_delete_query, (id,))
-            # commit the changes to the database
-            connection.commit()
-            print("Sala eliminada con exito")
-        #if the sala is not available (disponible==False) then print a message
-        except mysql.connector.Error as error:
-            print("Error al eliminar la sala: {}".format(error))
-        #the conection to the database is closed after the operation is done
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                print("Conexión a MySQL cerrada")
+def eliminarSala(id_sala,disponible):
+    #if the sala is available (disponible==True) then change the state of estado to false 
+    if disponible:
+        # connect to the database
+        connection = mysql.connector.connect(host='localhost',
+                                             database='Gestion_Salas',
+                                             user="root",
+                                                password='1234qwer#')
+        cursor = connection.cursor()
+        # update the estado of the sala to false (eliminada)
+        consulta = "UPDATE salas SET estado = false WHERE id_sala = %s"
+        cursor.execute(consulta, (id,))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        print("La sala ha sido eliminada correctamente")
+    else:
+        print("La sala no se puede eliminar porque tiene reservas activas")
+        return False
+    return True
--- crearBD.py
+++ crearBD.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="1234pwc"
+  password="1234qwer#"
 )
 
 mycursor = mydb.cursor()
@@ -12,6 +12,8 @@ mycursor = mydb.cursor()
 def crearBD():
     mycursor.execute("CREATE DATABASE IF NOT EXISTS Gestion_Salas")
     mycursor.execute("USE Gestion_Salas")
-    mycursor.execute("CREATE TABLE salas (id_sala INTEGER PRIMARY KEY AUTO_SUMARY, nombre VARCHAR(6) CHECK(nombre LIKE 'Sala %', capacidad INTEGER), estatus BOOLEAN)")
+    mycursor.execute("CREATE TABLE salas (id_sala INTEGER PRIMARY KEY AUTO_SUMMARY, nombre VARCHAR(6) CHECK(nombre LIKE 'Sala %', capacidad INTEGER), estatus BOOLEAN)")
     mycursor.execute("CREATE TABLE reserva (id_sala INTEGER, id_usuario INTEGER, inicio DATETIME, fin DATETIME, activa BOOLEAN,PRIMARY KEY(id_reserva,id_usuario)")
-    mycursor.execute("CREATE TABLE usuarios(id_usuario INTEGER PRIMARY KEY AUTO_SUMARY, nombre VARCHAR(50), contraseña VARCHAR(16))")
+    mycursor.execute("CREATE TABLE usuarios(id_usuario INTEGER PRIMARY KEY AUTO_SUMMARY, nombre VARCHAR(50), contraseña VARCHAR(16))")
+
+crearBD()
