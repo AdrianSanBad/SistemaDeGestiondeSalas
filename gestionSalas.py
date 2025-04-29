019e9d6c5ec8a15bb9e046370ada2894af2ef729
SergioCO-ESCOM <56370198+SergioCO-ESCOM@users.noreply.github.com>
2025-04-29 10:59:33 -0600

    Creación de base de datos

---
+++ gestionSalas.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+usuario = 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS Gestion_Salas")
